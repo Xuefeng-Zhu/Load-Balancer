5d87818a287ff12ae0ba247fa348b6cde7379e63
dpetrisko <dan.petrisko@outlook.com>
2015-04-15 22:53:13 -0500

    Hardware Monitor implemented and tested.

--- HardwareMonitor.py
+++ HardwareMonitor.py
@@ -1,34 +1,53 @@
+import sched
+
 __author__ = 'Dan'
 
 import psutil
 import threading
+import time
 
 
 class HardwareMonitor(object):
     """ Monitors the system performance of the node, used for the transfer policy. """
-    SAMPLES_PER_100 = 5
+    SAMPLES_PER_SEC = 2
     NUM_SAMPLES = 10
 
     def __init__(self):
-        """ Initializes the sample collections. """
-        self.cpu_samples = [None] * HardwareMonitor.NUM_SAMPLES
+        """ Initializes the sample collections and threads necessary for scheduling. """
+        super(HardwareMonitor, self).__init__()
+        self.cpu_samples = [0.0] * HardwareMonitor.NUM_SAMPLES
         self.current_sample = 0
+        self.monitor_thread = None
+        self.monitoring = False
 
-        self.timer = threading.Timer(100.0 * 1000, self.cpu_record())
+    def start(self):
+        """ Starts the monitor thread and prevents it from ending without manually calling stop. """
+        self.monitoring = True
+        self.monitor_thread = threading.Thread(target=self.record_cpu)
+        self.monitor_thread.start()
 
-    def cpu_record(self):
+    def stop(self):
+        """ Causes the monitor thread to finish on next execution.  Safer than killing the thread outright. """
+        self.monitoring = False
+
+    def record_cpu(self, scheduler=None):
         """ Performs a system call to record another sample of cpu utilization. """
-        self.cpu_samples[self.current_sample] = psutil.cpu_percent()
-        self.current_sample = (self.current_sample + 1) % HardwareMonitor.NUM_SAMPLES
+        if scheduler is None:
+            scheduler = sched.scheduler(time.time, time.sleep)
+            scheduler.enter(0, 1, self.record_cpu, [scheduler])
+            scheduler.run()
+        else:
+            self.cpu_samples[self.current_sample] = psutil.cpu_percent(0.1)
+            self.current_sample = (self.current_sample + 1) % HardwareMonitor.NUM_SAMPLES
 
-        self.timer = threading.Timer(100.0 * 1000, self.cpu_record())
+            if self.monitoring:
+                scheduler.enter(1.0 / HardwareMonitor.SAMPLES_PER_SEC / 1000, 1, self.record_cpu, [scheduler])
 
     def get_cpu_usage(self):
         """ Calculates and returns the average CPU usage over a specific period. """
         total = 0
+
         for sample in self.cpu_samples:
             total += sample
 
         return total / HardwareMonitor.NUM_SAMPLES
-
-
--- tests/HardwareMonitorTest.py
+++ tests/HardwareMonitorTest.py
@@ -1,7 +1,10 @@
 __author__ = 'Dan'
 
 import unittest
+import time
 
+from Job import Job
+from WorkerThread import WorkerThread
 from HardwareMonitor import HardwareMonitor
 
 
@@ -9,11 +12,36 @@ class HardwareMonitorUsageTest(unittest.TestCase):
     def testHardwareMonitorTracksReasonableCpuUsage(self):
         monitor = HardwareMonitor()
 
-        #cpu_usage = monitor.get_cpu_usage()
-        #time.sleep(0.5)
+        monitor.start()
+        time.sleep(1)
+        monitor.stop()
 
-        #assert cpu_usage > 0
-        #assert cpu_usage < 10.0
+        cpu_usage = monitor.get_cpu_usage()
+
+        assert cpu_usage > 0
+        assert cpu_usage < 100.0
+
+    def testHardwareMonitorCpuUsageIncreasesWithWorkload(self):
+        work_data = [1.111111] * 1000
+        test_job = Job(work_data)
+        worker_thread = WorkerThread()
+        monitor = HardwareMonitor()
+
+        monitor.start()
+        time.sleep(1)
+        clean_cpu_usage = monitor.get_cpu_usage()
+
+        worker_thread.current_job = test_job
+        worker_thread.run()
+
+        time.sleep(1)
+        dirty_cpu_usage = monitor.get_cpu_usage()
+
+        monitor.stop()
+        while worker_thread.is_alive():
+            pass
+
+        assert dirty_cpu_usage > clean_cpu_usage
 
 
 if __name__ == '__main__':

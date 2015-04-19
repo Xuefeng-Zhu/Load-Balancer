0f33e78c83f42e42c34f5f0689f37df5c7eb8d50
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-04-19 16:44:59 -0500

    add job and state

---
+++ state.py
@@ -0,0 +1,11 @@
+__author__ = 'Xuefeng Zhu'
+
+
+class State:
+	"""
+	Store the node state information
+	"""
+    def __init__(self):
+        self.num_jobs = 0
+        self.throttling = 0
+        self.cpu_usage = 0

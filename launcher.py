ca0df450a062c9f81cf8f5d8919b0517f52be50c
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-04-20 13:15:12 -0500

    update launcher

--- launcher.py
+++ launcher.py
@@ -1,3 +1,5 @@
+import json
+import sys
 from Queue import Queue
 from job import Job
 from worker_thread import WorkerThread
@@ -12,7 +14,10 @@ NUM_JOB = 1024
 
 
 class Launcher:
-    def __init__(self, remote_ip):
+    def __init__(self, is_local, remote_ip, vector):
+        self.is_local = is_local
+        self.vector = vector
+
         self.job_queue = Queue()
         self.work_thread = WorkerThread(self.job_queue)
         self.hardware_monitor = HardwareMonitor(self.work_thread)
@@ -22,31 +27,55 @@ class Launcher:
                                self.transfer_manager, self.state_manager, self.hardware_monitor)
 
     def bootstrap(self):
+        if self.is_local:
+            self.allocate_jobs()
+            self.transfer_jobs()
+
         self.hardware_monitor.start()
         self.transfer_manager.receive_job()
         self.state_manager.receive_state()
 
-    def allocate_jobs(self, vector):
+    def allocate_jobs(self):
 
-        job_size = len(vector) / NUM_JOB
+        job_size = len(self.vector) / NUM_JOB
         for i in range(NUM_JOB):
             pos = i * job_size
-            job = Job(pos, vector[pos, pos + job_size])
+            job = Job(pos, self.vector[pos, pos + job_size])
             self.job_queue.put(job)
 
     def transfer_jobs(self):
-        for i in range(NUM_JOB/2):
+        for _ in range(NUM_JOB / 2):
             job = self.job_queue.get()
             self.transfer_manager.send_job(job)
 
 
-if __name__ == '__main__':
-    vector = [1.111111] * 1024 * 1024 * 32
-    remote_ip = "1.1.1.1"
+def load_config():
+    with open('config.json') as f:
+        return json.load(f)
 
-    launcher = Launcher(remote_ip)
-    launcher.allocate_jobs(vector)
-    launcher.transfer_jobs()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "Usage: python launcher.py L/R"
+        exit(0)
+
+    if sys.argv[1] == "L":
+        is_local = True
+    elif sys.argv[1] == "R":
+        is_local = False
+    else:
+        print "Please provide valid argument"
+        exit(0)
+
+    config = load_config()
+    if is_local:
+        remote_ip = config["Remote"]
+        vector = [1.111111] * 1024 * 1024 * 32
+    else:
+        remote_ip = config["Local"]
+        vector = None
+
+    launcher = Launcher(is_local, remote_ip, vector)
     launcher.bootstrap()
 
     launcher.work_thread.join()
